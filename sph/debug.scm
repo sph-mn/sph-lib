9384197f996b21608b8c6a8abc828079a84221b9
sph <sph@posteo.eu>
2015-10-18 16:52:49 +0000

    -

--- sph/debug.scm
+++ sph/debug.scm
@@ -7,20 +7,18 @@
     (guile)
     (ice-9 pretty-print)
     (rnrs base)
-    (only (guile) basename simple-format)
-    (only (sph one) pass)
     (sph)
     (system vm program)
     (system vm trace)
-    (system vm vm))
+    (system vm vm)
+    (only (guile) basename simple-format)
+    (only (sph one) pass))
 
   (define (trace . procs)
-    (each
-      (l (ele) (trace-calls-to-procedure ele #:width 200 #:prefix "")) procs))
+    (each (l (ele) (trace-calls-to-procedure ele #:width 200 #:prefix "")) procs))
 
   (define (trace-in . procs)
-    (each
-      (l (ele) (trace-calls-in-procedure ele #:width 200 #:prefix "")) procs))
+    (each (l (ele) (trace-calls-in-procedure ele #:width 200 #:prefix "")) procs))
 
   (set-vm-trace-level! (the-vm) 1)
   (debug-set! show-file-name (quote base)))
