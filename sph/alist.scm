32b8354af2575930ce360d449b63f746410d5aff
sph <sph@posteo.eu>
2015-10-31 14:07:51 +0000

    add symbol-alist-bind-and*

--- sph/alist.scm
+++ sph/alist.scm
@@ -39,7 +39,7 @@
     set-alist-bindings!
     symbol-alist
     symbol-alist-bind
-    symbol-alist-bind-and
+    symbol-alist-bind-and*
     symbol-alist-ref
     symbol-alist-select-apply)
   (import
@@ -120,9 +120,9 @@
   (define-syntax-rule (symbol-alist-bind alist (key ...) body ...)
     ((lambda (key ...) body ...) (alist-ref alist (quote key)) ...))
 
-  (define-syntax-rule (symbol-alist-bind-and alist (key ...) body ...)
-    ;body is only evaluated if all alist values are true
-    (symbol-alist-bind alist (key ...) (if (and key ...) (begin body ...) #f)))
+  (define-syntax-rule (symbol-alist-bind-and* alist (key ...) body ...)
+    ;alist values are bound in order of keys, and false is returned if any key value is false
+    (and-let* ((key (alist-ref alist (quote key))) ...) (begin body ...)))
 
   (define-syntax-rule (bindings->alist identifier ...)
     ;create an alist with keys named like the identifiers and values from identified variables
