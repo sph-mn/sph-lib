4f39891a01e603c14f133565a8faa184f6efda17
sph <sph@posteo.eu>
2015-11-12 14:28:04 +0000

    all tests pass

--- sph/base64.scm
+++ sph/base64.scm
@@ -37,9 +37,8 @@
     (rnrs))
 
   (define *table*
-    #(vector #\A #\B #\C #\D #\E #\F #\G #\H #\I #\J #\K #\L #\M #\N #\O #\P #\Q #\R #\S #\T #\U #\V
-      #\W #\X #\Y #\Z #\a #\b #\c #\d #\e #\f #\g #\h #\i #\j #\k #\l #\m #\n #\o #\p #\q #\r #\s #\t
-      #\u #\v #\w #\x #\y #\z #\0 #\1 #\2 #\3 #\4 #\5 #\6 #\7 #\8 #\9 #\+ #\/))
+    (quote
+      #(#\A #\B #\C #\D #\E #\F #\G #\H #\I #\J #\K #\L #\M #\N #\O #\P #\Q #\R #\S #\T #\U #\V #\W #\X #\Y #\Z #\a #\b #\c #\d #\e #\f #\g #\h #\i #\j #\k #\l #\m #\n #\o #\p #\q #\r #\s #\t #\u #\v #\w #\x #\y #\z #\0 #\1 #\2 #\3 #\4 #\5 #\6 #\7 #\8 #\9 #\+ #\/)))
 
   (define (base64-encode-port iport oport)
     (define (put-alphabet i) (put-char oport (vector-ref *table* i))) (assert (binary-port? iport))
