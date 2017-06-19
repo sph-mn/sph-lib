b43bf603ee913b89a2e259beb7ff5897ca1d75e5
sph <sph@posteo.eu>
2017-06-19 20:40:24 +0000

    add library description to (sph hashtable)

--- modules/sph/hashtable.scm
+++ modules/sph/hashtable.scm
@@ -1,6 +1,5 @@
-; (sph hashtable) - rnrs-hashtable processing
 ; written for the guile scheme interpreter
-; Copyright (C) 2010-2016 sph <sph@posteo.eu>
+; Copyright (C) 2010-2017 sph <sph@posteo.eu>
 ; This program is free software; you can redistribute it and/or modify it
 ; under the terms of the GNU General Public License as published by
 ; the Free Software Foundation; either version 3 of the License, or
@@ -37,6 +36,7 @@
     hashtable-q-ref
     hashtable-q-set-multiple
     hashtable-ref
+    hashtable-select
     hashtable-set-multiple
     hashtable-tree-merge
     hashtable-tree-merge!
@@ -47,6 +47,7 @@
     hashtables-set!
     list->hashtable
     rnrs-hashtable-ref
+    sph-hashtable-description
     string-hashtable
     symbol-hashtable)
   (import
@@ -66,6 +67,8 @@
     (only (sph vector) vector-each vector-each-with-index)
     (rename (rnrs hashtables) (hashtable-ref rnrs-hashtable-ref)))
 
+  (define sph-hashtable-description "rnrs-hashtable processing")
+
   (define* (alist->hashtable a #:optional (equal-proc equal?) (hash-proc equal-hash))
     "convert assoc-list/alist \"a\" to an r6rs hashtable"
     (let ((ht (make-hashtable hash-proc equal-proc)))
@@ -73,8 +76,8 @@
 
   (define (hashtable . associations)
     "{key value} ... -> hashtable
-    creates a hashtable.
-    example: (hashtable 'a 1 'b 2 'c 3)"
+     creates a hashtable.
+     example: (hashtable 'a 1 'b 2 'c 3)"
     (list->hashtable associations))
 
   (define-syntax-rules symbol-hashtable
@@ -102,12 +105,12 @@
 
   (define (hashtable-copy-empty a)
     "hashtable -> hashtable
-    creates a new empty hashtable with the same equivalence and hash function as the input hashtable."
+     creates a new empty hashtable with the same equivalence and hash function as the input hashtable."
     (make-hashtable (hashtable-equivalence-function a) (hashtable-hash-function a)))
 
   (define (hashtable-each proc ht)
     "procedure:{key value ->} hashtable ->
-    call proc for each key and value association in hashtable"
+     call proc for each key and value association in hashtable"
     (let-values (((keys values) (hashtable-entries ht)))
       (vector-each-with-index (l (e index) (proc e (vector-ref values index))) keys)))
 
@@ -124,29 +127,29 @@
 
   (define (hashtable-map proc a)
     "procedure:{key value -> value} hashtable -> hashtable
-    set a new value for every key value association in hash"
+     set a new value for every key value association in hash"
     (let (r (hashtable-copy a)) (hashtable-map! proc r) r))
 
   (define (hashtable-make-immutable a)
     "hashtable -> hashtable
-    results in a new hashtable that is an immutable version of the given one.
-    this does not affect nested hashtables"
+     results in a new hashtable that is an immutable version of the given one.
+     this does not affect nested hashtables"
     (hashtable-copy a #f))
 
   (define (hashtable-invert a)
     "hashtable -> hashtable
-    use values as keys and keys as values"
+     use values as keys and keys as values"
     (let (r (hashtable-copy-empty a)) (hashtable-each (l (k v) (hashtable-set! r v k)) a)))
 
   (define (hashtable-invert! a)
     "hashtable -> hashtable
-    use values as keys and keys as values"
+     use values as keys and keys as values"
     (hashtable-each (l (k v) (hashtable-set! a v k)) a))
 
   (define (hashtable-key-proc ht)
     "hashtable -> procedure:{value -> key}
-    results in a procedure that caches the preparations for creating its result
-    so it is much more efficient for repeated calls."
+     results in a procedure that caches the preparations for creating its result
+     so it is much more efficient for repeated calls."
     (call-with-values (nullary (hashtable-entries ht))
       (l (keys values)
         (let (values-list (vector->list values))
@@ -159,7 +162,7 @@
 
   (define (hashtable-merge! a b)
     "hashtable hashtable -> unspecified
-    copy the values of hash b to hash a. key values are overwritten."
+     copy the values of hash b to hash a. key values are overwritten."
     (call-with-values (nullary (hashtable-entries b))
       (l (keys values)
         (vector-each-with-index (l (key index) (hashtable-set! a key (vector-ref values index)))
@@ -167,13 +170,13 @@
 
   (define (hashtable-merge a b)
     "hashtable hashtable -> hashtable
-    like hashtable-merge! but not side-effecting. it instead works on a copy of hash a and results in it"
+     like hashtable-merge! but not side-effecting. it instead works on a copy of hash a and results in it"
     (let (r (hashtable-copy a #t)) (hashtable-merge! r b) r))
 
   (define (hashtable-tree-merge! a b)
     "hashtable hashtable -> unspecified
-    like hashtable-merge!, but for keys that occur in both hashtables and which have hashtables as value
-    hashtable-tree-merge! is called to merge the hashtables instead of just overwriting the value in a."
+     like hashtable-merge!, but for keys that occur in both hashtables and which have hashtables as value
+     hashtable-tree-merge! is called to merge the hashtables instead of just overwriting the value in a."
     (call-with-values (nullary (hashtable-entries b))
       (l (keys values)
         (vector-each-with-index
@@ -186,7 +189,7 @@
 
   (define (hashtable-tree-merge a b)
     "hashtable hashtable -> hashtable
-    like hashtable-tree-merge! but not side-effecting. it instead works on a copy of hashtable and results in it"
+     like hashtable-tree-merge! but not side-effecting. it instead works on a copy of hashtable and results in it"
     (let (r (hashtable-copy a #t)) (hashtable-tree-merge! r b) r))
 
   (define (hashtable-set-multiple ht . assoc)
@@ -199,7 +202,7 @@
 
   (define (hashtable-update-multiple! ht keys proc)
     "hashtable list procedure:{any:values ... -> (any:new-values ...)} -> hashtable
-    set values for \"keys\" in hashtable to new values by mapping using \"proc\""
+     set values for \"keys\" in hashtable to new values by mapping using \"proc\""
     (each (l (k v) (hashtable-set! ht k v)) keys
       (apply proc (map (l (e) (hashtable-ref ht e)) keys))))
 
@@ -220,8 +223,8 @@
 
   (define* (hashtable->alist ht #:optional (depth 0))
     "rnrs-hashtable [integer] -> alist
-    converts a hashtable to an alist. if depth is greater than 0 any other
-    hashtables being values up to this nesting depth will be converted too."
+     converts a hashtable to an alist. if depth is greater than 0 any other
+     hashtables being values up to this nesting depth will be converted too."
     (hashtable-fold
       (l (key value r)
         (pair
@@ -247,8 +250,10 @@
 
   (define* (list->hashtable a #:optional (equal-proc equal?) (hash-proc equal-hash))
     "convert a list to an r6rs standard library hashtable. nested lists are not converted to a hash.
-    example
-    (hashtable-ref (list->hashtable (list 'a 1 'b 2)) 'b #f)
-    -> 2"
+     example
+     (hashtable-ref (list->hashtable (list 'a 1 'b 2)) 'b #f)
+     -> 2"
     (let ((ht (make-hashtable hash-proc equal-proc)))
-      (fold (l (e r) (if r (begin (hashtable-set! ht r e) #f) e)) #f a) ht)))
+      (fold (l (e r) (if r (begin (hashtable-set! ht r e) #f) e)) #f a) ht))
+
+  (define (hashtable-select a . keys) (map (l (b) (hashtable-ref a b)) keys)))
