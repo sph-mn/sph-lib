b589d4892117794ca0e06ae553e30da8215052bf
sph <sph@posteo.eu>
2017-08-30 10:31:30 +0000

    -

--- modules/sph/lang/config.scm
+++ modules/sph/lang/config.scm
@@ -1,5 +1,6 @@
 (library (sph lang config)
   (export
+    config-write
     config-read
     config-read-file
     config-read-string)
@@ -18,10 +19,10 @@
      * all elements are read as being quasiquoted
      * unquote can be used to create dynamically calculated results
      * hashtables (eventually nested) are the parsing result
-     the file content or string is parsed with scheme read as a quasiquoted list.
+     the file content or string is parsed with scheme read elements of a quasiquoted list.
      in that list and sub-lists, expressions stand for key and value alternatingly.
      lists and sub-lists become hashtables, with the following exception:
-     sub-lists that begin with a period are returned as lists with the period removed
+     sub-lists that begin with a double period are returned as lists with the double period removed
      # example file content
        default-type \"text\"
        mode development
@@ -29,18 +30,18 @@
        preview-image-size (unquote (+ 255 1))
        browse
        ( page-size 75
-         include-types (. \"itml\" \"plaintext\"))
+         include-types (.. \"itml\" \"plaintext\"))
        other #(1 2 4)")
 
   (define config-env (environment (q (sph))))
 
-  (define (config-read port)
+  (define* (config-read port #:optional (eval-environment config-env))
     (tree-map-lists-and-self
       (l (a)
         (if (null? a) (ht-make-eq 0)
           (if (eq? (q ..) (first a)) (tail a) (ht-from-list a eq? ht-hash-symbol))))
-      (eval (list (q quasiquote) (port->datums port)) config-env)))
+      (eval (list (q quasiquote) (port->datums port)) eval-environment)))
 
-  (define (config-write port) "")
-  (define (config-read-string a) (call-with-input-string a config-read))
-  (define (config-read-file a) (call-with-input-file a config-read)))
+  (define (config-write a port) "experimental" (each (l (a) (write a port) (newline)) (ht-alist a (inf))))
+  (define (config-read-string a . b) (call-with-input-string a (l (a) (applly config-read a b))))
+  (define (config-read-file a . b) (call-with-input-file a (l (a) (apply config-read a b)))))
