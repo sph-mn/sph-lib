c72e5a18d532cd33cc16ef91ed335692d0272821
sph <sph@posteo.eu>
2019-01-01 00:34:37 +0000

    add f32vector/f64vector map-with-index

--- modules/sph/uniform-vector.scm
+++ modules/sph/uniform-vector.scm
@@ -12,10 +12,13 @@
     f32vector-map!
     f32vector-map-with
     f32vector-map-with!
+    f32vector-map-with-index
+    f32vector-map-with-index!
     f32vector-range-map
     f32vector-range-map!
     f32vector-range-map-with
     f32vector-range-map-with!
+    f32vector-range-map-with-index!
     f32vector-range-set
     f32vector-range-set!
     f64vector-copy
@@ -28,10 +31,13 @@
     f64vector-map!
     f64vector-map-with
     f64vector-map-with!
+    f64vector-map-with-index
+    f64vector-map-with-index!
     f64vector-range-map
     f64vector-range-map!
     f64vector-range-map-with
     f64vector-range-map-with!
+    f64vector-range-map-with-index!
     f64vector-range-set
     f64vector-range-set!
     integer->bytevector
@@ -189,6 +195,33 @@
     (define (id f variable start end a . b)
       (apply vector-range-map (l a (apply f variable a)) start end a b)))
 
+  (define-syntax-rule (define-uv-map-with-index! id vector-length vector-range-map!)
+    (define (id f a . b)
+      "procedure:{any:element ... -> any} xvector ... -> unspecified
+       like uv-map but modifies \"a\""
+      (apply vector-range-map! a f 0 (- (vector-length a) 1) a b)))
+
+  (define-syntax-rule
+    (define-uv-map-with-index id vector-copy-zero* vector-range-map! vector-length)
+    (define (id f a . b)
+      "procedure:{index any:element ... -> any} xvector ... -> xvector
+       call f for each element of each vector. (f index a-element b-element ...)"
+      (vector-copy-zero* a
+        (l (result) (apply vector-range-map! result f 0 (- (vector-length a) 1) a b)))))
+
+  (define-syntax-rule (define-uv-range-map-with-index! id vector-set! vector-ref)
+    (define (id result f start end a . b)
+      "procedure:{integer:index any:element ... -> any} integer integer xvector xvector:source ... -> unspecified
+       set result to the map results of calling f for each element between start and end of one or multiple xvectors.
+       f is called (f index a-element b-element ...).
+       all vectors must be of sufficient size"
+      (let loop ((index start))
+        (if (<= index end)
+          (begin
+            (vector-set! result index
+              (apply f index (vector-ref a index) (map (l (a) (vector-ref a index)) b)))
+            (loop (+ 1 index)))))))
+
   ; f64
   (define-uv-copy f64vector-copy make-f64vector f64vector-length 8)
   (define-uv-copy-zero f64vector-copy-zero make-f64vector f64vector-length)
@@ -206,6 +239,14 @@
   (define-uv-range-map-with! f64vector-range-map-with! f64vector-range-map!)
   (define-uv-range-set! f64vector-range-set! f64vector-set!)
   (define-uv-range-set f64vector-range-set f64vector-copy* f64vector-range-set!)
+
+  (define-uv-map-with-index! f64vector-map-with-index! f64vector-length
+    f64vector-range-map-with-index!)
+
+  (define-uv-map-with-index f64vector-map-with-index f64vector-copy-zero*
+    f64vector-range-map-with-index! f64vector-length)
+
+  (define-uv-range-map-with-index! f64vector-range-map-with-index! f64vector-set! f64vector-ref)
   ;
   ; f32
   (define-uv-copy f32vector-copy make-f32vector f32vector-length 4)
@@ -224,6 +265,14 @@
   (define-uv-range-map-with! f32vector-range-map-with! f32vector-range-map!)
   (define-uv-range-set! f32vector-range-set! f32vector-set!)
   (define-uv-range-set f32vector-range-set f32vector-copy* f32vector-range-set!)
+
+  (define-uv-map-with-index! f32vector-map-with-index! f32vector-length
+    f32vector-range-map-with-index!)
+
+  (define-uv-map-with-index f32vector-map-with-index f32vector-copy-zero*
+    f32vector-range-map-with-index! f32vector-length)
+
+  (define-uv-range-map-with-index! f32vector-range-map-with-index! f32vector-set! f32vector-ref)
   ;
   ; s32
   (define-uv-copy s32vector-copy make-s32vector s32vector-length 4)
